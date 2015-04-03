80e129ce59ce0d3fb401b246f57981d42e3bb099
luis gallet <nanogallet@gmail.com>
2015-04-03 16:50:55 -0400

    corrected Tower manager class

--- src/main/model/tower/TowerManager.java
+++ src/main/model/tower/TowerManager.java
@@ -1,5 +1,37 @@
 package main.model.tower;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+
 public class TowerManager {
 
+	private List<Tower> towers = new ArrayList<Tower>();
+	
+
+   public TowerManager() {
+	   towers = new LinkedList<Tower>();
+	
+   }
+
+   public void addTower(Tower t) {
+		towers.add(t);
+	}
+	
+	public void removeTower(Tower t) {
+		towers.remove(t);
+	}
+	
+	public Tower getTower(Tower t){
+		
+		int index = towers.indexOf(t);
+						
+		return towers.get(index);
+		
+		
+	}
+	
 }

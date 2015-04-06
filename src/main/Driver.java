b5a16cfd3d926bf1980810c794bc5baf1158cebf
Justin Bell <justin.bell@mail.mcgill.ca>
2015-04-06 00:00:15 -0400

    a file was deleted

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -6,26 +6,12 @@ import javax.swing.SwingUtilities;
 import main.controller.PurchaseController;
 import main.model.Player;
 import main.view.PurchaseView;
-import main.view.TDPanel;
 
 public class Driver {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame;
-				frame = new JFrame("TD");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);           
-				frame.setSize(200,200);        
-				frame.setVisible(true);
-				
-				PurchaseView PV = new PurchaseView();
-				
-				Player p = new Player(100, 1000, 1);
-				p.addObserver(PV);
-				
-				PurchaseController PC = new PurchaseController(PV, p);
-				
-				frame.add(PV);
+		
 			}
 	});
 	}
--- src/main/view/TDPanel.java
+++
@@ -1,9 +0,0 @@
-package main.view;
-
-import javax.swing.JPanel;
-
-import main.model.IObserver;
-
-public abstract class TDPanel extends JPanel implements IObserver {
-
-}
